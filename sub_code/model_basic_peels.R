9edcc313bfb5c55a19cc6a4c4c88c8ed352f6f08
Jonathan Bourne <cheg_a_leg@hotmail.com>
2020-04-27 06:41:59 +0100

    fix after SETSe update. Peels mean tension per node
    This update fixes all the embedding code after the major update to the way that SETse functions.
    I also added in a change to the way the the Peels quintet is projected. Tension is no mean per node. this is a much better way of reflecting the relationship of nodes with thier neighbours

--- sub_code/model_basic_peels.R
+++ sub_code/model_basic_peels.R
@@ -3,10 +3,10 @@
 
 #create the dataset to test the nearest neighbours
 modelling_peels_df <- peel_strain_sum %>% ungroup %>%
-  select(mean_strain, mean_height = mean, Network) 
+  select(mean_tension_per_node, mean_abs_elevation, graph_type) 
 
 #create the formula to use
-mod_form <- as.formula(Network ~ mean_strain + mean_height)
+mod_form <- as.formula(graph_type ~ mean_tension_per_node + mean_abs_elevation)
 
 #create the ten sets of repeat folds
 #make a large number of repeats to be able to get distribution
@@ -20,38 +20,38 @@ knn_holdout_results <- function(splits, k, ...) {
   # Save the 10%
   holdout <- assessment(splits)
   # Fit the model to the 90%
-  mod <- class::knn(train = train_df %>% select(-Network), 
-                    cl = train_df$Network, 
-                    test = holdout%>% select(-Network), k = k)
+  mod <- class::knn(train = train_df %>% select(-graph_type), 
+                    cl = train_df$graph_type, 
+                    test = holdout%>% select(-graph_type), k = k)
   
   # `augment` will save the predictions with the holdout data set
   res <- holdout %>% mutate(pred = factor(mod, levels = c("A", "B", "C", "D", "E")),
-                            Network = factor(Network, levels = c("A", "B", "C", "D", "E")))
+                            graph_type = factor(graph_type, levels = c("A", "B", "C", "D", "E")))
   
   #Output the accuracy of the model
-  accuracy(res, Network, pred)
+  accuracy(res, graph_type, pred)
 }
 
 #This function will return the accuracy of the logistic regression
-log_holdout_results <- function(splits, ...) {
-  train_df = analysis(splits)
-  
-  # Save the 10%
-  holdout <- assessment(splits)
-  # Fit the model to the 90%
-  mod <- vglm(Network ~., family=multinomial, data = train_df)
-  
-  # `augment` will save the predictions with the holdout data set
-  res <- predict(mod, holdout, type="response") %>% apply(., 1, which.max)
-  
-  res <- holdout %>%
-    mutate(pred =  predict(mod, holdout, type="response") %>% 
-             apply(., 1, which.max)%>% LETTERS[.] %>% factor(., levels = c("A", "B", "C", "D", "E")),
-           Network = factor(Network, levels = c("A", "B", "C", "D", "E")))
-  
-  #Output the accuracy of the model
-  accuracy(res, Network, pred)
-}
+# log_holdout_results <- function(splits, ...) {
+#   train_df = analysis(splits)
+#   
+#   # Save the 10%
+#   holdout <- assessment(splits)
+#   # Fit the model to the 90%
+#   mod <- vglm(Network ~., family=multinomial, data = train_df)
+#   
+#   # `augment` will save the predictions with the holdout data set
+#   res <- predict(mod, holdout, type="response") %>% apply(., 1, which.max)
+#   
+#   res <- holdout %>%
+#     mutate(pred =  predict(mod, holdout, type="response") %>% 
+#              apply(., 1, which.max)%>% LETTERS[.] %>% factor(., levels = c("A", "B", "C", "D", "E")),
+#            Network = factor(Network, levels = c("A", "B", "C", "D", "E")))
+#   
+#   #Output the accuracy of the model
+#   accuracy(res, Network, pred)
+# }
 
 nearest_k_results <-1:10 %>% map_df(~{
   test2 <- map2_df(.x = rs_obj$splits, 
@@ -63,15 +63,15 @@ nearest_k_results <-1:10 %>% map_df(~{
 })
 
 
-log_reg_results <-rs_obj$splits %>% map_df(~{
-  test2 <- log_holdout_results(.x,  mod_form)
-  
-  tibble(accuracy =mean(test2$.estimate))
-  
-})
-
-#aggregate by the number of repeats
-boot_res <- log_reg_results %>%
-  mutate(v = rep(1:100, each = 10)) %>%
-  group_by(v) %>%
-  summarise(accuracy = mean(accuracy))
+# log_reg_results <-rs_obj$splits %>% map_df(~{
+#   test2 <- log_holdout_results(.x,  mod_form)
+#   
+#   tibble(accuracy =mean(test2$.estimate))
+#   
+# })
+# 
+# #aggregate by the number of repeats
+# boot_res <- log_reg_results %>%
+#   mutate(v = rep(1:100, each = 10)) %>%
+#   group_by(v) %>%
+#   summarise(accuracy = mean(accuracy))
--- sub_code/process_peel_strain.R
+++ sub_code/process_peel_strain.R
@@ -53,25 +53,34 @@ k_options2 <- k_options %>%
   select(-k_fract) %>%
   pivot_wider(., names_from = k_id,  values_from = k)
 
+ 
 
+#This adds in lots of data to the edges of the network.
+#The node data allows the edges to be assigned to a within group between group class.
+#the k values are then assigned based on the edge class
 1:500 %>% walk(~{
   start_time <- Sys.time()
   
+  #take the instance of randomly generated Peels quintet graph
   graph_ref <- .x
   g_out <- multi_quintet[[graph_ref]]
   
+  #get the name of the graph type A to E
   graph_type <- graph_attr(g_out, "type")
   
+  #convert to a list of dataframes
   g_df <- as_data_frame(g_out, what = "both")
   
-  edge_df <- left_join(
+  #The first two joins merge the node data onto the edges for the "from" and "to" nodes, this gives the group and sub group data
+  #The third join adds in the k values
+  edge_df <- left_join( #first join
     g_df$edges %>% mutate(edge_name = paste(from, to, sep = "_"),
                           from = as.character(from),
                           to = as.character(to)),
     g_df$vertices %>%
       rename(from_class = class, from_sub_class = sub_class),
     by = c("from"="node")) %>%
-    left_join(.,
+    left_join(.,    #second join
               g_df$vertices %>%
                 rename(to_class = class, to_sub_class = sub_class),
               by = c("to"="node")) %>%
@@ -81,37 +90,73 @@ k_options2 <- k_options %>%
         from_class == to_class ~ "class",
         TRUE ~ "inter"
       ),
-      flow = 1,
+    #  flow = 1, no longer necessary due to upgraded SETSe
       distance = 1,
-      capacity = 1
+    #  capacity = 1
     ) %>%
-    left_join(k_options2, by = "match_type")
+    left_join(k_options2, by = "match_type") #third and final join
   
+  #Remake the graph including the addition edge data
   g_out <-graph_from_data_frame(edge_df, directed = FALSE,  g_df$vertices %>% select(node, class, sub_class) %>%
                                   mutate(force = ifelse(class=="A", 1/20, -1/20))) #Forces sum to 1
   
   means_df <-0:6 %>% map_df(~{
     
+    
+    
     save_path <- file.path(PLwd, "peel_strain_files", paste0("graph_ref_", graph_ref, "_k_", .x, ".rds") )
 if(!file.exists(save_path)){    
-    setse_complete <- auto_SETSe(g_out,
-                                 force = "force",
-                                 flow = "flow",
-                                 distance = "distance",
-                                 capacity = "capacity",
-                                 edge_name = "edge_name",
-                                 k = paste0("k_", .x),
-                                 tol = 40/10000)
+  setse_complete <- auto_SETSe(g_out,
+                               force = "force",
+                               distance = "distance",
+                               edge_name = "edge_name",
+                               k = paste0("k_", .x),
+                               tol = sum(abs(vertex_attr(g_out, "force")))/10000,
+                               sparse = FALSE, #The networks are small so dense networks are faster approx x3 in this case
+                               verbose = F)
     
-    setse_complete$graphsummary <-tibble(
+    
+#The biconnected components don't work due to a naming error why?   
+
+  # SETSe_bicomp2(.,
+  #               force = "force",
+  #               distance = "distance",
+  #               edge_name = "edge_name",
+  #               k = "k",
+  #               tstep = 0.02,
+  #               tol = sum(abs(vertex_attr(., "force")))/10000,
+  #               static_limit = 1,
+  #               sparse = F,
+  #               verbose = TRUE)
+  
+  #by comparing the average tension experienced by each node we can 
+  edge_tension_per_node <- setse_complete$edge_embeddings %>%
+    select(edge_name, tension) %>%
+    separate(col = edge_name, into = c("from", "to"), sep = "_") %>%
+    pivot_longer(cols =c(from, to), names_to = "type", values_to = "node") %>%
+    group_by( node) %>%
+    summarise(mean_tension_per_node = mean(tension),
+              total_edges = n()) %>%
+    summarise(mean_tension_per_node = mean(mean_tension_per_node)) #this takes advantage of the skew in the mean to aid differentiation
+  #some nodes may have the same total tension but spread across different amount of nodes
+  
+  node_details <- left_join(setse_complete$node_embeddings, g_df$vertices, by = "node")
+  
+    
+    setse_complete$graphsummary <- tibble(
       strain = mean(abs(setse_complete$edge_embeddings$strain)),
-      elevation = left_join(setse_complete$node_embeddings, g_df$vertices, by = "node") %>% 
+      mean_A_elevation = node_details %>% 
         filter(class=="A") %>%
         pull(elevation) %>% mean,
+      mean_abs_elevation = mean(abs(node_details$elevation)),
+      median_abs_elevation =  median(abs(node_details$elevation)),
+      euc_elevation = sqrt(sum(node_details$elevation^2)),
       tension = mean(abs(setse_complete$edge_embeddings$tension)),
-      residual_force =  mean(abs(setse_complete$node_embeddings$static_force)), 
-      k = .x) %>%
-      mutate(graph_type = graph_type,
+      residual_force =  mean(abs(setse_complete$node_embeddings$static_force))) %>%
+      bind_cols(edge_tension_per_node ) %>%
+      mutate(
+        k =.x,
+        graph_type = graph_type,
              graph_id = graph_ref)
     
     saveRDS(setse_complete, save_path)
